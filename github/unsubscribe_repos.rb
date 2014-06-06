1f9713851d80492db1fbb8bc27bb26982ea1c85d
Christopher Webber <cwebberops@gmail.com>
2014-06-06 14:42:50 -0700

    Actually unsubscribe all the things

--- github/unsubscribe_repos.rb
+++ github/unsubscribe_repos.rb
@@ -2,8 +2,34 @@
 
 require 'octokit'
 
+WHITELIST = [
+ %r{opscode-cookbooks/},
+ %r{cwebberOps/},
+ %r{opsallthethings/},
+ %r{puppetbestpractices/},
+ %r{hangops/},
+ %r{opscode/supermarket},
+ %r{ladevops/}
+]
+
 github_user = Octokit::Client.new(:access_token => ENV['GITHUB_API_KEY'])
 github_user.auto_paginate = true
 
 subscriptions = github_user.subscriptions()
 
+subscriptions.each do |sub|
+
+  unsub = true
+  WHITELIST.each do |whitelist|
+
+    if whitelist =~ sub[:full_name]
+      unsub = false
+    end
+  end
+
+  if unsub
+    if github_user.delete_subscription(sub[:full_name])
+      puts "Unsubscribed from #{sub[:full_name]}"
+    end
+  end
+end
